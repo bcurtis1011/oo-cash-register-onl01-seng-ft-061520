c49856f2be2ec3ba993bad9aa093eb1c9a6a1684
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-30 00:50:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,39 @@
-# class CashRegister
+class CashRegister
   
   
-#   def initialize(discount=nil)
-#     self.total = 0
-#     self.discount = discount if discount != nil
-#     self.purchases = []
+  def initialize(discount=nil)
+    self.total = 0
+    self.discount = discount if discount != nil
+    self.purchases = []
     
-#   end
+  end
   
-#   def add_item()
+  def add_item()
     
-#   end
-# end
+  end
+end
+
+
+
+# CashRegister
+#   ::new
+#     sets an instance variable @total on initialization to zero (FAILED - 1)
+#     optionally takes an employee discount on initialization (FAILED - 2)
+#   #total
+#     returns the current total (FAILED - 3)
+#   #add_item
+#     accepts a title and a price and increases the total (FAILED - 4)
+#     also accepts an optional quantity (FAILED - 5)
+#     doesn't forget about the previous total (FAILED - 6)
+#   #apply_discount
+#     the cash register was initialized with an employee discount
+#       applies the discount to the total price (FAILED - 7)
+#       returns success message with updated total (FAILED - 8)
+#       reduces the total (FAILED - 9)
+#     the cash register was not initialized with an employee discount
+#       returns a string error message that there is no discount to apply (FAILED- 10)
+#   #items
+#     returns an array containing all items that have been added (FAILED - 11)
+#   #void_last_transaction
+#     subtracts the last item from the total (FAILED - 12)
+#     returns the total to 0.0 if all items have been removed (FAILED - 13)
